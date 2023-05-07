ba79cbb2c49f45733f8f8c6a01b3b9d59713f378
sobunkohla <sobunkohla@gmail.com>
2023-05-07 06:36:40 +0200

    finishing the crash course

---
+++ app/posts/new/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>new post</div>
+  )
+}
+
+export default page
---
+++ app/posts/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>posts</div>
+  )
+}
+
+export default page
